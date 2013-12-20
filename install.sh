1565d1d9515f7b2070c2b16da8e9951fc06d98cc
Srivatsa Katta <vatsa.katta@gmail.com>
2013-12-20 23:42:16 +0530

    bash autocompletion for Linux and Darwin environments

--- install.sh
+++ install.sh
@@ -7,3 +7,15 @@ if [ ! -f /usr/local/bin/riakcli ]; then
 	ln -sf $PWD/riakcli /usr/local/bin/riakcli
 	echo "INFO: 'riakcli' added to '/usr/local/bin'"
 fi
+
+
+platform=`uname`
+bash_completion_dir="/etc/bash_completion.d"
+if [[ "${platform}" == 'Linux' ]]; then
+	bash_completion_dir=/etc/bash_completion.d
+elif [[ "${platform}" == 'Darwin' ]]; then
+	bash_completion_dir=/usr/local/etc/bash_completion.d
+fi
+
+cp -f riakcli_bashcompletion ${bash_completion_dir}/riakcli_bashcompletion
+echo "INFO: Installing bash_completion for riakcli in to ${bash_completion_dir}"
